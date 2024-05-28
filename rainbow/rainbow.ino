a6ee20e270b13160b6449b78380cb65650b981a3
dj-viking <anders.swedishviking@gmail.com>
2024-05-28 01:28:47 -0400

    update rainbow

--- rainbow/rainbow.ino
+++ rainbow/rainbow.ino
@@ -4,12 +4,15 @@
  * @brief Empty constructor; init pins/strand length/data order later:
  */
 
-auto strip = Adafruit_WS2801();
 
 void setup()
 {
+  Adafruit_WS2801 strip = Adafruit_WS2801();
   // TODO: find out the clock pin on the actual board
   strip.updatePins(6, 7);
+  strip.updateLength(1);
+  strip.updateOrder(WS2801_RGB);
+
   strip.begin();
   strip.show();
   Serial.begin(9600);
