eab42b34d2008dfac7b5aaacb69ff221d8107718
dj-viking <anders.swedishviking@gmail.com>
2024-06-23 23:05:07 -0400

    update switch code

--- midioutputswitch/midi_output_switch/midi_output_switch.ino
+++ midioutputswitch/midi_output_switch/midi_output_switch.ino
@@ -4,7 +4,7 @@
 int datapin = 6;
 
 // analog pin
-int analogpin = 5; // not sure yet
+int analogpin = A0;
 
 byte status_byte = B11000010;
 byte data_byte = B00101100;
@@ -17,8 +17,7 @@ void setup()
 
   // set up pins
   pinMode(datapin, OUTPUT);
-  // pinMode(interruptpin, INPUT_PULLUP);
-  // attachInterrupt(digitalPinToInterrupt(interruptpin), inter_handler, RISING);
+  pinMode(analogpin, INPUT_PULLUP); // pulled high to start
 }
 
 void loop()
@@ -31,6 +30,21 @@ void loop()
   // and then send the bytes down the data pin
   // hmmm
 
+  Serial.print("analog pin reading\n");
+  Serial.print(analogRead(analogpin));
+  int tolerance = 0.13;
+  // analog input is started with pulled high.
+  // the switch is pulling it low with 220ohm to ground
+  while(analogRead(analogpin) <= (21 + tolerance)) {
+    Serial.print("\nSEND THE MIDI NOWWWWWW\n");
+    Serial.print("\n");
+    Serial.print("\n");
+    delay(1);
+  }
+  Serial.print("\n");
+  delay(100);
+  Serial.print("\n");
+  Serial.print("===================\n");
   /**
    * if (readpin(analogpin) === HIGH) {
    *     digitalWrite(datapin, status_byte);
